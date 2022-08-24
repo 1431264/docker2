e45d30b13958a39f6789dce6a427b2e106e952e0
1431264 <80273241+1431264@users.noreply.github.com>
2022-08-24 17:36:09 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export RIG_NAME=${RIG_NAME:-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8 ; echo '')'~ethash'}
-/gminer/miner --algo $MINING_ALGO --server "$MINING_POOL" --user "${WALLET_ADDRESS:-"88yUzYzB9wrR2r2o1TzXxDMENr6Kbadr3caqKTBUNFZ3dWVt6sJcpWBAwMwNRtEi7nHcBcqzmExNfdNK7ughaCeUFuXXpPp"}" \
+/gminer/miner --algo $MINING_ALGO --server "$MINING_POOL" --user "${WALLET_ADDRESS:-"846eg2pWgP7DsF6yHV9vfFfQQC3KWz3HB9RudyZcPe1vYp7SMj799Dufs34bALJtnvUvqhSCUS3sy88p3CCXMgcCRFqstWw"}" \
   --pass "$RIG_NAME" --proto stratum
